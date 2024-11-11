47b1cffa77a25cc205bc7a317b01e3a3b81d1d10
dieudv <dieu.cit@gmail.com>
2024-11-11 08:34:46 +0700

    Use SurfaceView instead of VideoView

--- app/src/main/java/org/lzdev/oldtv/MainActivity.kt
+++ app/src/main/java/org/lzdev/oldtv/MainActivity.kt
@@ -3,31 +3,33 @@ package org.lzdev.oldtv
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.AlertDialog
-import android.content.*
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.media.MediaPlayer
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.net.Uri
 import android.net.wifi.WifiManager
 import android.os.Bundle
-import android.util.Log
 import android.view.KeyEvent
+import android.view.SurfaceView
 import android.webkit.WebResourceRequest
 import android.webkit.WebResourceResponse
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.ArrayAdapter
-import android.widget.VideoView
 import org.intellij.lang.annotations.Language
-
 
 class MainActivity : Activity() {
     private val channels: ArrayList<Channel> = ArrayList()
     private var index = 0
     private lateinit var webView: WebView
     private var currentVideoURL = ""
-    private lateinit var videoView: VideoView
+    private lateinit var surfaceView: SurfaceView
     private var isChannelLoaded = false
+    private var mediaPlayer: MediaPlayer? = null
 
     private var mWifiReceiver: BroadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
@@ -53,7 +55,7 @@ class MainActivity : Activity() {
 
     private val mOnErrorListener: MediaPlayer.OnErrorListener = MediaPlayer.OnErrorListener { _, _, _ ->
         if (isNetworkConnected()) {
-            videoView.start()
+            mediaPlayer?.start()
         }
         true
     }
@@ -63,7 +65,7 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        videoView = findViewById(R.id.video_view)
+        surfaceView = findViewById(R.id.surface_view)
 
         WebView.setWebContentsDebuggingEnabled(true)
         webView = WebView(this)
@@ -71,19 +73,14 @@ class MainActivity : Activity() {
         webView.settings.userAgentString = getString(R.string.user_agent)
         webView.webViewClient = object : WebViewClient() {
 
-            override fun shouldInterceptRequest(
-                view: WebView?, request: WebResourceRequest?
-            ): WebResourceResponse? {
+            override fun shouldInterceptRequest(view: WebView?, request: WebResourceRequest?): WebResourceResponse? {
                 runOnUiThread {
-                    if (request != null) {
-                        val url = request.url.toString()
-                        if (url.indexOf(".m3u8") > 0) {
-                            if (currentVideoURL != url) {
-                                Log.e(TAG, "request URL = ${request.url}")
-                                currentVideoURL = url
-                                videoView.setVideoURI(Uri.parse(url))
-                                videoView.start()
-                            }
+                    request?.let {
+                        val url = it.url.toString()
+                        if (url.contains(".m3u8") && currentVideoURL != url) {
+                            currentVideoURL = url
+                            val headers = it.requestHeaders // Capture all request headers
+                            playVideoWithHeaders(url, headers)
                         }
                     }
                 }
@@ -93,12 +90,11 @@ class MainActivity : Activity() {
             override fun onPageFinished(view: WebView?, url: String?) {
                 if (!isChannelLoaded) {
                     view?.evaluateJavascript(jsCode) {
-                        if (it != null && it.indexOf("http") >= 0) {
+                        if (it != null && it.contains("http")) {
                             val text = it.replace("\"", "")
                             channels.clear()
                             val channelsText = text.split("|")
                             for (line in channelsText) {
-                                Log.e(TAG, line)
                                 channels.add(Channel(line.split(",")[0], line.split(",")[1]))
                             }
                             isChannelLoaded = true
@@ -112,29 +108,27 @@ class MainActivity : Activity() {
 
         registerWifiReceiver()
 
-        videoView.setOnErrorListener(mOnErrorListener)
         if (isNetworkConnected()) {
             webView.loadUrl("https://vtvgo.vn")
         }
     }
 
-    private fun isNetworkConnected(): Boolean {
-        var result = false
-        val cm = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        cm.run {
-            cm.getNetworkCapabilities(cm.activeNetwork)?.run {
-                when {
-                    hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
-                        result = true
-                    }
-
-                    hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> {
-                        result = true
-                    }
-                }
-            }
+    private fun playVideoWithHeaders(url: String, headers: Map<String, String>) {
+        mediaPlayer?.release()
+        mediaPlayer = MediaPlayer().apply {
+            setDataSource(this@MainActivity, Uri.parse(url), headers)
+            setDisplay(surfaceView.holder)
+            setOnPreparedListener { start() }
+            setOnErrorListener(mOnErrorListener)
+            prepareAsync()
         }
-        return result
+    }
+
+    private fun isNetworkConnected(): Boolean {
+        val cm = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        return cm.getNetworkCapabilities(cm.activeNetwork)?.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) == true ||
+                cm.getNetworkCapabilities(cm.activeNetwork)
+                    ?.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) == true
     }
 
     private fun registerWifiReceiver() {
@@ -146,7 +140,6 @@ class MainActivity : Activity() {
     }
 
     override fun onKeyUp(keyCode: Int, event: KeyEvent?): Boolean {
-        Log.d(TAG, "onKeyUp $keyCode")
         when (keyCode) {
             KeyEvent.KEYCODE_DPAD_UP -> {
                 if (index < channels.size - 1) {
@@ -168,10 +161,7 @@ class MainActivity : Activity() {
 
             KeyEvent.KEYCODE_DPAD_CENTER -> {
                 val builder: AlertDialog.Builder = AlertDialog.Builder(this)
-                val adapter = ArrayAdapter<String>(this, android.R.layout.select_dialog_item)
-                channels.forEach {
-                    adapter.add(it.name)
-                }
+                val adapter = ArrayAdapter(this, android.R.layout.select_dialog_item, channels.map { it.name })
                 builder.setSingleChoiceItems(adapter, index) { dialog, which ->
                     index = which
                     playChannel()
@@ -191,10 +181,9 @@ class MainActivity : Activity() {
         if (index >= channels.size) {
             index = 0
         }
-        if (channels.size == 0) {
+        if (channels.isEmpty()) {
             webView.loadUrl("https://vtvgo.vn")
         } else {
-            Log.e(TAG, channels[index].url)
             webView.loadUrl(channels[index].url)
         }
         return true
@@ -202,12 +191,12 @@ class MainActivity : Activity() {
 
     override fun onPause() {
         super.onPause()
-        videoView.pause()
+        mediaPlayer?.pause()
     }
 
     override fun onResume() {
         super.onResume()
-        videoView.start()
+        mediaPlayer?.start()
     }
 
     companion object {
